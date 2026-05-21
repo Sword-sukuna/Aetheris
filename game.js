2d476955ce9203a765f3f506d5342200543c5788
Raul Rosa <109114162+Sword-sukuna@users.noreply.github.com>
2026-05-21 16:37:22 -0300

    teste v1

--- game.js
+++ game.js
@@ -22,8 +22,10 @@ const game = new Phaser.Game(config);
 
 let player;
 let cursors;
-let slimes;
 let spaceKey;
+let slimes;
+
+const TILE = 32;
 
 function preload() {
     this.load.image("player", "https://labs.phaser.io/assets/sprites/phaser-dude.png");
@@ -33,53 +35,59 @@ function preload() {
 
 function create() {
 
-    // chão simples
-    for (let y = 0; y < 15; y++) {
-        for (let x = 0; x < 25; x++) {
-            this.add.image(x * 32, y * 32, "tile").setOrigin(0);
+    // câmera suave seguindo player
+    this.cameras.main.setBounds(0, 0, 2000, 2000);
+
+    // chão (grid organizado)
+    for (let y = 0; y < 40; y++) {
+        for (let x = 0; x < 60; x++) {
+            this.add.image(x * TILE, y * TILE, "tile")
+                .setOrigin(0)
+                .setAlpha(0.9);
         }
     }
 
-    // player
-    player = this.physics.add.sprite(100, 100, "player");
-    player.setScale(2);
+    // player centralizado no mundo
+    player = this.physics.add.sprite(400, 300, "player");
+    player.setScale(1.6);
+    player.setCollideWorldBounds(true);
+
+    this.cameras.main.startFollow(player, true, 0.08, 0.08);
 
     // slimes
     slimes = this.physics.add.group();
 
-    slimes.create(300, 200, "slime").setScale(1.5);
-    slimes.create(500, 300, "slime").setScale(1.5);
-    slimes.create(700, 150, "slime").setScale(1.5);
+    for (let i = 0; i < 6; i++) {
+        let slime = slimes.create(
+            300 + i * 120,
+            200 + (i % 2) * 120,
+            "slime"
+        );
 
-    // controles
+        slime.hp = 3;
+        slime.setScale(1.4);
+        slime.setTint(0x88ff88);
+    }
+
     cursors = this.input.keyboard.createCursorKeys();
     spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 }
 
 function update() {
 
-    const speed = 200;
+    const speed = 220;
 
     player.setVelocity(0);
 
-    if (cursors.left.isDown) {
-        player.setVelocityX(-speed);
-    }
+    if (cursors.left.isDown) player.setVelocityX(-speed);
+    if (cursors.right.isDown) player.setVelocityX(speed);
+    if (cursors.up.isDown) player.setVelocityY(-speed);
+    if (cursors.down.isDown) player.setVelocityY(speed);
 
-    if (cursors.right.isDown) {
-        player.setVelocityX(speed);
-    }
-
-    if (cursors.up.isDown) {
-        player.setVelocityY(-speed);
-    }
-
-    if (cursors.down.isDown) {
-        player.setVelocityY(speed);
-    }
-
-    // ataque (SPACE)
+    // ataque mais RPG
     if (Phaser.Input.Keyboard.JustDown(spaceKey)) {
+
+        let hit = false;
 
         slimes.children.iterate(slime => {
 
@@ -90,9 +98,40 @@ function update() {
                 slime.x, slime.y
             );
 
-            if (dist < 80) {
-                slime.destroy();
+            if (dist < 70) {
+
+                slime.hp--;
+
+                // feedback visual
+                slime.setTint(0xff5555);
+
+                this.time.delayedCall(120, () => {
+                    if (slime && slime.active) {
+                        slime.setTint(0x88ff88);
+                    }
+                });
+
+                hit = true;
+
+                if (slime.hp <= 0) {
+
+                    this.tweens.add({
+                        targets: slime,
+                        alpha: 0,
+                        scale: 0,
+                        duration: 200,
+                        onComplete: () => slime.destroy()
+                    });
+                }
             }
         });
+
+        if (!hit) {
+            player.setTint(0xff4444);
+
+            this.time.delayedCall(100, () => {
+                player.clearTint();
+            });
+        }
     }
 }
