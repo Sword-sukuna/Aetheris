b7ce79b51891c5292fd5f91ae3223b28d53007d0
Raul Rosa <109114162+Sword-sukuna@users.noreply.github.com>
2026-05-21 13:04:42 -0300

    ..

--- game.js
+++ game.js
@@ -1,1 +1,18 @@
+const player = document.getElementById("player");
 
+let x = 100;
+let y = 100;
+
+document.addEventListener("keydown",(e)=>{
+
+    if(e.key==="w") y -= 10;
+
+    if(e.key==="s") y += 10;
+
+    if(e.key==="a") x -= 10;
+
+    if(e.key==="d") x += 10;
+
+    player.style.left = x + "px";
+    player.style.top = y + "px";
+});
